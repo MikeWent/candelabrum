671a63fdf79f812d48ad73ff43bfd369658afbf5
Mike_Went <git@meew.me>
2020-07-03 12:51:22 +0300

    Fix bugs with pulse mode and adapter

--- candelabrum.py
+++ candelabrum.py
@@ -22,29 +22,26 @@ for mac in args.mac:
     except Exception as e:
         print(f"unable to connect: {e}")
         continue
-    
-    try:
-        if args.intensity != None:
-            if args.intensity > 0 and args.intensity <= 100:
-                print(f"set intensity: {args.intensity}")
-                # turn candela on
-                device.char_write_handle(0x001f, bytearray([0x43, 0x40, 0x01]))
-                # set light intensity
-                device.char_write_handle(0x001f, bytearray([0x43, 0x42, args.intensity]))
-            else:
-                print("lamp off")
-                # set zero light intensity
-                device.char_write_handle(0x001f, bytearray([0x43, 0x42, 0]))
-                # turn candela off
-                device.char_write_handle(0x001f, bytearray([0x43, 0x40, 0x02]))
 
-        if args.pulse:
-            print("pulse on")
+    if args.intensity != None:
+        if args.intensity > 0 and args.intensity <= 100:
+            print(f"set intensity: {args.intensity}")
             # turn candela on
             device.char_write_handle(0x001f, bytearray([0x43, 0x40, 0x01]))
-            # # set 1/100 light intensity
-            # device.char_write_handle(0x001f, bytearray([0x43, 0x42, 1]))
-            # enable pulse mode
-            device.char_write_handle(0x001f, bytearray([0x43, 0x67, 0x02]))
-    finally:
-        adapter.stop()
+            # set light intensity
+            device.char_write_handle(0x001f, bytearray([0x43, 0x42, args.intensity]))
+        else:
+            print("lamp off")
+            # set zero light intensity
+            device.char_write_handle(0x001f, bytearray([0x43, 0x42, 0]))
+            # turn candela off
+            device.char_write_handle(0x001f, bytearray([0x43, 0x40, 0x02]))
+
+    if args.pulse:
+        print("pulse on")
+        # turn candela off
+        device.char_write_handle(0x001f, bytearray([0x43, 0x40, 0x02]))
+        # turn candela on
+        device.char_write_handle(0x001f, bytearray([0x43, 0x40, 0x01]))
+        # enable pulse mode
+        device.char_write_handle(0x001f, bytearray([0x43, 0x67, 0x02]))
