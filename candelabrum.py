cd2255fa73502b65fa5e28110fe0703ad10a204c
Mike_Went <git@meew.me>
2020-07-20 10:48:52 +0300

    Fix issues with several connections and Python 3.7

--- candelabrum.py
+++ candelabrum.py
@@ -6,7 +6,7 @@ import pygatt
 
 # get arguments from command line
 p = argparse.ArgumentParser()
-p.add_argument("--mac", "-m", help="Bluetooth MAC address of Yeelight Candela", type=str, action="extend", nargs="+", required=True)
+p.add_argument("--mac", "-m", help="Bluetooth MAC address of Yeelight Candela", type=str, nargs="*", required=True)
 g = p.add_mutually_exclusive_group()
 g.add_argument("--intensity", "-i", help="Light intensity (0-100)", type=int, required=False)
 g.add_argument("--pulse", "-p", help="Enable pulse (flickering) mode", action="store_true", required=False)
@@ -45,3 +45,5 @@ for mac in args.mac:
         device.char_write_handle(0x001f, bytearray([0x43, 0x40, 0x01]))
         # enable pulse mode
         device.char_write_handle(0x001f, bytearray([0x43, 0x67, 0x02]))
+
+    device.disconnect()
